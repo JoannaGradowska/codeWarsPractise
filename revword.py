fd173c65ff5736012b6727d22075740d407bbfea
Joanna Gradowska <joanna.gradowska@icloud.com>
2021-12-17 12:32:40 +0100

    ver 1

---
+++ revword.py
@@ -0,0 +1,11 @@
+# Complete the function that accepts a string parameter,
+# and reverses each word in the string. All spaces in the string should be retained.
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(str):
+    new = []
+    for i in str.split(' '):
+        new.append(i[::-1])
+    return ' '.join(new)
